99936c70dc6e05479300ede5ce9fda885bc53630
Shawn T <shawn@huthunt.com>
2018-01-24 13:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@
 
   function attributesToPhrase(drivers){
 return drivers.map(function(driver){
-  let driverName = driver.name
-  let driverHometown = driver.hometown
+  let driverName = driver[name]
+  let driverHometown = driver[hometown]
 
   return '${driverName} is from ${driverHometown}'
 })
